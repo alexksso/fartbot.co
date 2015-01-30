e188e18cd46bab5de63e68c7022f69a9c8cc8b73
Alexander <alex@ksso.net>
2015-01-30 15:43:37 +0100

    project cleanup

--- mobile/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/bin/AndroidManifest.xml : \
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/AndroidManifest.xml \
--- mobile/platforms/android/bin/Fartbot-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/bin/Fartbot-debug-unaligned.apk : \
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/bin/Fartbot.ap_ \
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/bin/classes.dex \
-/Users/alex/Documents/Development/fartbot.co/mobile/platforms/android/libs/cordova-3.0.0.jar \
